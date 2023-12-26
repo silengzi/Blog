77b751ac3d8083d6d828cf58f3ee0c0a169c27f8
silengzi <474960201@qq.com>
2023-12-26 17:01:47 +0800

    header

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <Header></Header>
+    <img src="@/assets/imgs/lay.jpg" alt="">
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    position: absolute;
+    top: 0;
+    z-index: -1;
+  }
+</style>
